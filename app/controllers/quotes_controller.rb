1ab85d7027ff2ddff5ff93654f48400e9f6f19f6
Aditi Green <asaki444@gmail.com>
2017-08-12 15:01:24 -0400

    worked on adding routes to the quotes controller, and forms

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,25 @@
 class QuotesController < ApplicationController
+    
+    def new
+        @quote = Quote.new
+    end
+    
+    def create
+        quote = Quote.create(quote_params)
+        redirect_to quote_path(quote)
+    end
+
+    def show 
+        @quote = Quote.find_by(:id params[:id])
+    end 
+
+    def create
+        quote = Quote.create(quote_params)
+        redirect_to quote_path(quote)
+    end
+    private
+
+    def quote_params
+      params.require(:quote).permit(:name, category_attributes: [:name], category_ids: [])
+    end
 end
