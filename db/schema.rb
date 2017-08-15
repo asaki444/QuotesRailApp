f13cfe760a0a465e43aaa13d93496d0dbc7569d5
Aditi Green <asaki444@gmail.com>
2017-08-14 23:36:14 -0400

    made changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814045235) do
-
-  create_table "authors", force: :cascade do |t|
-    t.string "name"
-    t.integer "quote_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170812143814) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
