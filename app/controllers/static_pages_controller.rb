1ee90da562c05266ef208a9540331a86ef11db93
Aditi Green <asaki444@gmail.com>
2017-10-18 21:14:19 -0400

    fixed the most relatable quote method if there are no quotes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     @quotes = Quote.all
-    @relatable = Quote.find_by(id: CategoryQuote.most_categories)
+    if @quotes != []
+      @relatable = Quote.find_by(id: CategoryQuote.most_categories)
+    end
   end
 end
