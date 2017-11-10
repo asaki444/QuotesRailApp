02f1380e933324d2c294d2239383a71c30c1e30c
Aditi Green <asaki444@gmail.com>
2017-11-09 21:23:27 -0500

    moved quote code to the javascript pipeline

---
+++ app/assets/javascripts/quote.js
@@ -0,0 +1,29 @@
+
+
+    class Quote {
+        constructor(data) {
+            this.text = data.attributes.text 
+            this.author = data.attributes.author
+            this.id = data.attributes.id
+        }
+
+        formatQuote() {
+            return (`
+                <h3> ${this.text} </h3>
+                <h4> - ${this.author}</h4> 
+                <a href="quotes/${this.id}">See Quote </a>
+            `)
+        }
+    }
+    
+$(document).ready(function() {
+   $.get('/quotes.json').done(function(data){
+      let text = "";
+      for (let i = 0; i < data.data.length; i++) {
+        const quote = new Quote(data.data[i]);
+        text += quote.formatQuote();
+      }
+         
+      $("div#quotes").html(text);
+    })
+}) 
